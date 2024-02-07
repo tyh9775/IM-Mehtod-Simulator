8d586d1c1fb80df0a8013d2f24f34f8141053ce8
tyh9775 <tyh0249@wmich.edu>
2024-02-07 11:01:25 -0500

    code

---
+++ Delta_Reader.py
@@ -0,0 +1,8 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.optimize import curve_fit
+from scipy.signal import argrelextrema, peak_widths
+#import scipy.stats as st
+from scipy.integrate import simpson
+#from scipy.interpolate import interp1d
+import random
